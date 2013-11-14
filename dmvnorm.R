aaa3216bd07241fc8ca84f79369796f4a791381d
scristia <sc759@cornell.edu>
2013-11-13 20:40:34 -0500

    Improved dmvnorm.R and fastlm.R

--- dmvnorm.R
+++ dmvnorm.R
@@ -1,24 +1,26 @@
 dmvnorm <- function(x, mu, S, log = TRUE) {
     ## x = data vector, mu = mean, S = covariance matrix
+    ## need: fast quadratic form
+    ## fast determinant of positive definite matrix
+
     ## if x numeric array, make row vector
     if(!is.matrix(x)) x <- matrix(x, nrow=1, ncol=ncol(S))
     k <- ncol(S)
-    ## need: fast quadratic form
-    ## fast determinant of positive definite matrix
+
+    ## check if positive definite
+    ## cheat by having chol check
     U <- try(chol(S), silent=TRUE)
     if(class(U) == "try-error") stop("S is not positive definite")
 
     d <- diag(U)
-    ## check if positive definite
+    ## determinant is squared product of diagonals from decomposition.
+    ## Do on log scale to avoid underflow.
     logd <- sum(log(d))
-    b <- crossprod(forwardsolve(U, t(x-mu), upper.tri=TRUE))
-#    b <- crossprod(backsolve(U, t(x-mu)))
+
+    ## quickly evaluate quadratic form
+    b <- crossprod(forwardsolve(t(U), t(x-mu)))
+    if(is.matrix(b)) b <- diag(b)
     logf  <- -k/2 * log(2*pi) - logd - 0.5 * b
     if(log) return(logf)
     else return(exp(logf))
-}
-
-is.pos.def <- function(x) {
-    ## check symmetric
-    return(sum(x == t(x)) == (nrow(x)^2))
 }
--- fastlm.R
+++ fastlm.R
@@ -6,46 +6,27 @@
 ##      na.rm
 ##########################################
 fastlm <- function(X, y, na.rm=FALSE) {
-    ## dumb way
     ## take advantage of X'X being symmetric
+    if(na.rm) {
+        if(any(is.na(X))) {
+            inx <- which(is.na(rowSums(X)))
+            X <- X[-inx,, drop=FALSE]
+            y <- y[-inx]
+        }
+        if(any(is.na(y))) {
+            iny <- which(is.na(y))
+            y <- y[-iny]
+            X <- X[-iny,, drop=FALSE]
+        }
+    }
+
     n <- nrow(X)
     p <- ncol(X)
     U <- chol(t(X)%*%X)
 
     b <- backsolve(U, forwardsolve(t(U), t(X)%*%y))
-    return(b)
-    #sigma <- 1/(n-p) * (t(y) %*% y - t(b) %*% t(X) %*% y)
-    #return(list("coefficients" = b, "vcov" = sigma))
+    sigma2 <- 1/(n-p) * (t(y) %*% y - t(b) %*% t(X) %*% y)
+    s2 <- diag(as.numeric(sigma2), p)
+    var.b <- backsolve(U, forwardsolve(t(U), s2))
+    return(list("coefficients" = b, "vcov" = var.b))
 }
-fastlm.svd <- function(X, y, na.rm=FALSE) {
-    ## dumb way
-    ## take advantage of X'X being symmetric
-    m <- svd(t(X)%*%X)
-    b <- m$v %*% diag(m$d^(-1)) %*% t(m$u) %*% t(X) %*% y
-
-    return(b)
-}
-fastlm.bad <- function(X, y, na.rm=FALSE) {
-    ## dumb way
-    ## take advantage of X'X being symmetric
-    b <- solve(t(X)%*%X, t(X)%*%y)
-
-    return(b)
-}
-system.time(fastlm(X,y))
-system.time(fastlm.svd(X,y))
-system.time(fastlm.bad(X,y))
-str(fastlm(X,y))
-ans <- solve(t(X)%*%X, t(X)%*%y)
-
-set.seed(2)
-## Generate predictor matrix
-n <- 100000
-p <- 500
-X <- cbind(1, matrix(rnorm(n * (p - 1)), n, p - 1))
-
-## Coefficents
-b <- rnorm(p)
-
-## Response
-y <- X %*% b + rnorm(n)
